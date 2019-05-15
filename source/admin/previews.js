53a8cf40c25e971f6646e4ba3eb01ddae9de4f6d
Dankore <zimmazone@yahoo.com>
2019-05-15 11:13:01 +0000

    delete blog preview

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -1,26 +1,3 @@
-const BlogPreview = ({ entry, widgetFor }) => {
-  const data = entry.get('data').toJS()
-  const date = dayjs(data.date).format('DD-MM-YY')
-  return (
-    <div className="news-detail">
-      <div className="container">
-        <div className="row">
-          <div className="title">
-            <span className="date">{date}</span>
-            <h1>{data.title}</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="image">
-            <img src={data.image}/>
-          </div>
-        </div>
-        <div className="row content">{widgetFor('body')}</div>
-      </div>
-    </div>
-  )
-}
-
 const ProductPreview = ({ entry, widgetFor }) => {
   const data = entry.get('data').toJS()
   return (
